8e9f9997e938fd76ceaa82da76c011e2a2c660a1
Jonas Bengtsson <jonas.b@gmail.com>
2023-01-25 21:27:21 +0100

    fix: Display error when trying to access dossier page in prod

--- components/DossierWebInterfacePage/DossierWebInterfacePage.tsx
+++ components/DossierWebInterfacePage/DossierWebInterfacePage.tsx
@@ -1,10 +1,12 @@
-import { NotificationContainer } from '@dossierhq/design';
+import { Message, NotificationContainer } from '@dossierhq/design';
 import '@dossierhq/design/main.css';
 import '@dossierhq/leaflet/icons.css';
 import '@dossierhq/leaflet/main.css';
 import '@dossierhq/react-components/main.css';
 import 'leaflet/dist/leaflet.css';
+import Link from 'next/link';
 import { ENABLE_WEB_INTERFACE } from '../../config/WebInterfaceConfig';
+import { FrontendUrls } from '../../utils/FrontendUrls';
 
 interface Props {
   children: React.ReactNode;
@@ -15,5 +17,11 @@ export function DossierWebInterfacePage({ children }: Props) {
     return <NotificationContainer>{children}</NotificationContainer>;
   }
 
-  return <></>;
+  return (
+    <Message color="warning">
+      <Message.Body>
+        Dossier is disabled in production. Go <Link href={FrontendUrls.home}>home</Link>.
+      </Message.Body>
+    </Message>
+  );
 }
