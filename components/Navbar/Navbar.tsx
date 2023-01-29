c34cdecc32d0cb5d0286bbcd471d5e0a5f3cd941
Jonas Bengtsson <jonas.b@gmail.com>
2023-01-29 19:14:27 +0100

    fix: Use a-links between dossier pages and non-dossier pages

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -11,6 +11,7 @@ interface Item {
   id: 'home' | 'csr' | 'ssr' | 'ssg' | 'admin-entities' | 'published-entities' | 'schema';
   url: string;
   title: string;
+  cssScope: 'app' | 'dossier';
 }
 
 const webInterfaceItems: Item[] = [
@@ -18,16 +19,19 @@ const webInterfaceItems: Item[] = [
     id: 'admin-entities',
     url: FrontendUrls.adminEntities,
     title: 'Entities',
+    cssScope: 'dossier',
   },
   {
     id: 'published-entities',
     url: FrontendUrls.publishedEntities,
     title: 'Published entities',
+    cssScope: 'dossier',
   },
   {
     id: 'schema',
     url: FrontendUrls.schemaEditor,
     title: 'Schema',
+    cssScope: 'dossier',
   },
 ];
 
@@ -36,32 +40,43 @@ const items: Item[] = [
     id: 'home',
     url: FrontendUrls.home,
     title: 'SSR app/',
+    cssScope: 'app',
   },
   {
     id: 'csr',
     url: FrontendUrls.clientSide,
     title: 'CSR',
+    cssScope: 'app',
   },
   {
     id: 'ssr',
     url: FrontendUrls.serverSide,
     title: 'SSR',
+    cssScope: 'app',
   },
   {
     id: 'ssg',
     url: FrontendUrls.static,
     title: 'SSG',
+    cssScope: 'app',
   },
   ...(ENABLE_WEB_INTERFACE ? webInterfaceItems : []),
 ];
 
 export function Navbar({ current }: Props) {
+  // Since the Dossier pages add global styles (especially css reset), we use Next Link between pages
+  // of the same scope, and regular <a> tags between pages of different scopes.
+  const currentCssScope = items.find((item) => item.id === current)?.cssScope;
   return (
     <nav className={styles.navbar}>
       <ul>
         {items.map((item) => (
           <li key={item.id} className={item.id === current ? styles.active : undefined}>
-            <Link href={item.url}>{item.title}</Link>
+            {item.cssScope === currentCssScope ? (
+              <Link href={item.url}>{item.title}</Link>
+            ) : (
+              <a href={item.url}>{item.title}</a>
+            )}
           </li>
         ))}
       </ul>
